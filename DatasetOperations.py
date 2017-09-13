bfc2194d8229ab68c29afd927bdc10934562fee9
MahmoudHigazy <ma7moudhigazy@gmail.com>
2017-09-13 20:30:53 +0200

    this code extracts features and labels from the images

--- DatasetOperations.py
+++ DatasetOperations.py
@@ -4,12 +4,15 @@ from pathlib import Path
 import tensorflow as tf
 import numpy as np
 X = []
+y = []
 
-pathphotos = Path("/home/ahmed/AgeRecognition/TrainData").glob('**/*.jpg')
-a=0
+pathphotos = Path("./TrainData").glob('**/*.jpg')
+
+#extract features and labels
 for ppath in pathphotos:
     #open photo as numpy array
-    im=np.asarray(Image.open(str(ppath)))
+    path = str(ppath)
+    im=np.asarray(Image.open(path))
     x = []
     #create list of every pixel in a single photo
     for i in range(im.shape[0]):
@@ -17,7 +20,4 @@ for ppath in pathphotos:
             x.append(im[i][j])
     #save the list of photo pixels to construct a matrix num_of_training_examples*num_of_pixels
     X.append(x)
-    #convert the list to a 2d numpy array
-final_array=np.array(X)
-
-print (final_array.shape)
+    y.append(int(path[len(path) - 8 : len(path) - 4]) - int(path[len(path) - 19 : len(path) - 15]))
