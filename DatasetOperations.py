6d03d6bf19bd51582ad3c2f276a1b4be54a22156
MahmoudHigazy <ma7moudhigazy@gmail.com>
2017-09-19 14:49:41 +0200

    tenserflow part done

--- DatasetOperations.py
+++ DatasetOperations.py
@@ -6,8 +6,8 @@ import numpy as np
 X = []
 y = []
 learning_rate = 0.1
+batch_size = 20
 training_epochs = 100
-batch_size = 256
 display_step = 1
 examples_to_show = 10
 
@@ -31,6 +31,8 @@ for ppath in pathphotos:
     y.append(int(path[len(path) - 8 : len(path) - 4]) - int(path[len(path) - 19 : len(path) - 15]))
 
 x=tf.placeholder("float",[None,n_input])
+
+
 input_to_hidden_encoder_w=tf.Variable(tf.random_normal([n_input,n_hidden_1]))
 input_to_hidden_encoder_b=tf.Variable(tf.random_normal([n_hidden_1]))
 
@@ -63,14 +65,39 @@ hidden_to_output_decode=tf.matmul(hidden_to_hidden_decode,hidden_to_output_decod
 hidden_to_output_decode=tf.nn.sigmoid(hidden_to_output_decode)
 
 
+# Prediction
+y_pred = hidden_to_output_decode
+# Targets (Labels) are the input data.
+y_true = x
 
+# Define loss and optimizer, minimize the squared error
+cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
-y_pred=hidden_to_output_decode
-y_true=x
+# Initializing the variables
+init = tf.global_variables_initializer()
 
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+    total_batch = int(len(X)/batch_size)
+    # Training cycle
+    for epoch in range(training_epochs):
+        # Loop over all batches
+        batch_num = 0
+        for i in range(total_batch):
+            batch_xs = []
+            #getting the batch number batch_num
+            index = batch_num * batch_size
+            for b in range(batch_size):
+                batch_xs.append(X[index])
+                index += 1
+            batch_num += 1
+            # Run optimization op (backprop) and cost op (to get loss value)
+            _, c = sess.run([optimizer, cost], feed_dict={x: batch_xs})
+        # Display logs per epoch step
+        if epoch % display_step == 0:
+            print("Epoch:", '%04d' % (epoch+1),
+                  "cost=", "{:.9f}".format(c))
 
-cost=tf.reduce_mean(tf.pow(y_true-y_pred,2))
-
-optimizer=tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
-
-init=tf.global_variables_initializer()
+    print("Optimization Finished!")
